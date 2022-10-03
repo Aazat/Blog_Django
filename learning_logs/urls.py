cc48c44a2650af54139f59d0f4250e3d5d89bf45
Aazar <shaikhaazar11@gmail.com>
2022-10-03 14:30:57 +0530

    User Login system and User Registration system added

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name= 'learning_logs'
 urlpatterns= [
 	path('',views.index, name='index'),	# Home page
-	path('topics/',views.topics, name='topics'),
-	path('topics/<int:topic_id>/',views.topic, name='topic')
+	path('topics/',views.topics, name='topics'),	# Topics Page
+	path('topics/<int:topic_id>/',views.topic, name='topic')	# Individual Topic Page
 ]
