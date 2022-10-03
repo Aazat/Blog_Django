de541843366406135b536283eda160775b0c06f5
Aazar <shaikhaazar11@gmail.com>
2022-10-03 13:24:12 +0530

    Topics and Entry pages and url patterns added and updated

---
+++ learning_logs/urls.py
@@ -0,0 +1,9 @@
+"""Defines url patterns for learning logs"""
+from django.urls import path
+from . import views
+app_name= 'learning_logs'
+urlpatterns= [
+	path('',views.index, name='index'),	# Home page
+	path('topics/',views.topics, name='topics'),
+	path('topics/<int:topic_id>/',views.topic, name='topic')
+]
